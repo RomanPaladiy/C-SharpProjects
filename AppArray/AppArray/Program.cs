4be4b10d89b7798332cf4fee6716b1599aaf5d0f
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-06-10 15:32:32 -0700

    Created arrays and lists

--- AppArray/AppArray/Program.cs
+++ AppArray/AppArray/Program.cs
@@ -10,9 +10,23 @@ namespace AppArray
     {
         static void Main(string[] args)
         {
+
+            // Creating an array of strings.
+            string[] names = { "Justin", "Richard", "Christopher", "Anthony" };
+            Console.WriteLine("Choose an Index of the array. 0-3");
+            int UserInput = Convert.ToInt32(Console.ReadLine());
+            if (UserInput > names.Length - 1)
+            {
+                // Error for index number chosen that doens't exist.
+                Console.WriteLine("That number does not exist in the index.");
+            }
+            else
+            {
+                Console.WriteLine(names[UserInput]);
+            }
+
+            
             // Creating an array/index.
-            Console.WriteLine("Choose an Index of the array. 0-5");
-            int numChosen = Convert.ToInt32(Console.ReadLine());
             int[] IntArray = new int[6];
             IntArray[0] = 10;
             IntArray[1] = 20;
@@ -20,21 +34,32 @@ namespace AppArray
             IntArray[3] = 40;
             IntArray[4] = 50;
             IntArray[5] = 60;
+            Console.WriteLine("Choose an Index of the array. 0-5");
+            int numChosen = Convert.ToInt32(Console.ReadLine());
+            if (numChosen > IntArray.Length - 1)
+            {
+                // Error for index number chosen that doens't exist.
+                Console.WriteLine("That number does not exist in the index.");
+            }
+            else
+            {
+            Console.WriteLine(IntArray[numChosen]);
+            }
 
-            // Error for index number chosen that doens't exist.
-            Console.WriteLine("That number does not exist in the index.");
 
             // Creating a list of strings.
             List<string> stringList = new List<string> {"string1", "string2", "string3", "string4"};
-
-            // Creating an array of strings.
-            string[] names = { "Justin", "Richard", "Christopher", "Anthony"};
-            Console.WriteLine(names[0]);
-
-
-
-
-
+            Console.WriteLine("Choose an Index of the list. 0-3");
+            UserInput = Convert.ToInt32(Console.ReadLine());
+            if (UserInput > stringList.Count -1)
+            {
+                // Error for index number chosen that doens't exist.
+                Console.WriteLine("That number does not exist in the index.");
+            }
+            else
+            {
+                Console.WriteLine(stringList[UserInput]);
+            }
             Console.ReadLine();
         }
     }
