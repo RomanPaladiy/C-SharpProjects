9aeb12df55ea6e392963fd501d0e458bf4a52adc
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-06-15 13:26:43 -0700

    Created Class and Methods

---
+++ CallingMethods/CallingMethods/Methods.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Methods
+    {
+        public int Multiplication(int userInput)
+        {
+            // Giving the instructions of the method and what to do when it's called.
+            int totalNum = userInput * 23;
+            Console.WriteLine("The product of " + userInput + " multiplied by 23 is: " + totalNum);
+            return totalNum;
+        }
+       
+        public int Addition(int userInput2)
+        {
+            // Giving the instructions for the Addition method when it's called.
+            int totalsum = userInput2 + 18;
+            Console.WriteLine("The sum of " + userInput2 + "and 18 is: " + totalsum);
+            return totalsum;
+        }
+   
+        public int Division(int userInput3)
+        {
+            // Giving the instructions for the Division method when it's called.
+            int totalquotient = userInput3 / 2;
+            Console.WriteLine("The quotient of " + userInput3 + "divided by 2 is: " + totalquotient);
+            return totalquotient;
+        }
+    }
+  
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompting user to provide an input in integer form.
+            Console.WriteLine("Please input an integer.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            // Instantiating the class and passing it to the mathObj
+            Methods mathObj = new Methods();
+
+            // Using the methods from the Methods Class and passing it the input from above code.
+            mathObj.Multiplication(input);
+            mathObj.Division(input);
+            mathObj.Addition(input);
+
+        }
+    }
+}
