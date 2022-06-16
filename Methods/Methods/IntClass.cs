9c41bc3cf8ecc9b47af2cd1b7de2b6875fb69319
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-06-15 19:27:16 -0700

    Optional Parameters

---
+++ Methods/Methods/IntClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class IntClass
+    {
+        // Passing parameters to the class and making one optional.
+        public int Addition(int numberOne , int numberTwo = 1)
+        {
+            // Creating the function of the method.
+            int totalSum = numberOne + numberTwo;
+            return totalSum;
+        }
+
+    }
+}
---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompting the user for one number to do the math equation with.
+            Console.WriteLine("Please input a number.");
+            int input = Convert.ToInt32(Console.ReadLine());
+            IntClass mathObject = new IntClass();
+            
+            // Implementing a try and catch block incase a number isn't entered.
+            try
+            {
+                Console.WriteLine("Please input a second number if you want.");
+                int input2 = Convert.ToInt32(Console.ReadLine());
+                int result = mathObject.Addition(input, input2);
+                Console.WriteLine(result);
+
+            }
+            catch
+            // If no number is entered for the second time it throws the default value set.
+            {
+                int result = mathObject.Addition(input);
+                Console.WriteLine(result);
+            }
+            Console.ReadLine();
+        }
+    }
+}
