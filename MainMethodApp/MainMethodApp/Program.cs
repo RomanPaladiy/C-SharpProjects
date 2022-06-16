fd997319fb17a53bba5b786f53023f0f7771cd05
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-06-15 18:40:03 -0700

    Class overload

---
+++ MainMethodApp/MainMethodApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompting the user to input an integer.
+            Console.WriteLine("Please provide an integer.");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            // Instantiating the class and passing it to mathObject.
+            mathMethods mathObject = new mathMethods();
+
+            // Calling the Addition method from the class mathMethods
+            int firstResult = mathObject.Addition(input);
+            Console.WriteLine(firstResult);
+            
+
+            // Prompting user for a decimal to then do a math operation with decimals.
+            Console.WriteLine("Please provide a decimal number.");
+            decimal input2 = Convert.ToDecimal(Console.ReadLine());
+            decimal secondResult = mathObject.Addition(input2);
+            Console.WriteLine(secondResult);
+
+            //Prompting user for a string to then do a math operation with it.
+            Console.WriteLine("Please provide a string.");
+            string input3 = Console.ReadLine();
+            string thirdResult = mathObject.Addition(input3);
+            Console.WriteLine(thirdResult);
+        }
+    }
+}           
---
+++ MainMethodApp/MainMethodApp/mathMethods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodApp
+{
+    class mathMethods
+    {
+        public int Addition(int input)
+        {
+            int totalSum = input + 5;
+            return totalSum;
+        }
+
+        public decimal Addition(decimal input)
+        {
+            int totalDiff = (int)(input - 10);
+            return totalDiff;
+        }
+
+        public string Addition(string input)
+        {
+            ;
+            string product = input + 10;
+            return product;
+        }
+
+
+    }
+}
