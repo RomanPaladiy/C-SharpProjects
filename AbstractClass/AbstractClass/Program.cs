062a56b269adddd1a0ceb1db6a98bea1249ed122
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-06-20 15:15:21 -0700

    Abstract Class

---
+++ AbstractClass/AbstractClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Instantiating the class 
+            Employee friend = new Employee();
+
+            // Passing in values for the properties.
+            friend.firstName = "Sample";
+            friend.lastName = "Student";
+
+            // Calling the Method from Person class.
+            friend.SayName();
+            Console.ReadLine();
+        }
+    }
+}
--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -10,22 +10,19 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            TwentyOneGame game = new TwentyOneGame();
+            game.Players = new List<string>() { "Jesse", "Bill", "Bob" };
+            game.ListPlayers();
             Console.ReadLine();
 
+            //Deck deck = new Deck();
+            //deck.Shuffle(3);
 
-            //// Or Game
-            //TwentyOneGame game = new TwentyOneGame();
-            //game.Players = new List<string>() { "Roman", "Jesse", "Bill" };
-            //game.ListPlayers();
-            //game.Play();
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
             //Console.ReadLine();
         }
 
