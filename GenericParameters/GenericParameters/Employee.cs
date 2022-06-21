4cf235f3c9fb53c6c401e37e54cae4bb8243876e
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-06-21 13:02:23 -0700

    Generic Uses

---
+++ GenericParameters/GenericParameters/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericParameters
+{
+    // Creating a generic class to let different datatypes be passed in.
+    public class Employee<T>
+    {
+        // Generic property to allow different datatypes.
+        public List<T> Things { get; set; }
+
+    }
+}
---
+++ GenericParameters/GenericParameters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating string type for Employee.(Passing in values)
+            Employee<string> Obj = new Employee<string>();
+            Obj.Things = new List<string>(){ "Bill", "Bob", "Jim"};
+
+            // Instantiating int type for Employee.(Passing in values)
+            Employee<int> Obj2 = new Employee<int>();
+            Obj2.Things = new List<int>() { 3, 5, 6, 7 };
+
+            // Creating a loop that prints out list of Things.
+            foreach (int thing2 in Obj2.Things)
+            {
+                Console.WriteLine(thing2);
+            }
+
+            foreach (string thing in Obj.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
