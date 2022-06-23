79ddced6e813043976bda76afbdeb8cad6fcd85b
RomanPaladiy <96402218+RomanPaladiy@users.noreply.github.com>
2022-06-22 17:32:14 -0700

    DateTime

---
+++ DateTimeApp/DateTimeApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using DateTime Now to get the content and using the "G" which is a format of time.
+            DateTime now = DateTime.Now;
+            string strDate = now.ToString("G");
+            Console.WriteLine(strDate);
+
+            // Prompting the user for input.
+            Console.WriteLine("Please input a number.");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            // Writing to the console to add hours based on the int the user gave above.
+            Console.WriteLine(now.AddHours(userInput));
+            Console.ReadLine();
+        }
+    }
+}
